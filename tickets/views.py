e5b4535c576ca5781b30d81b11a7fe56640574c7
inferiks <tsylyurik22@bk.ru>
2025-09-25 09:00:37 +0300

    add views.py

--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+from .models import Equipment, Tickets
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+# Equipment
+class EquipmentListView(LoginRequiredMixin, ListView):
+    model = Equipment
+    template_name = 'tickets/equipments_list.html'
+    
+class EquipmentDetailView(LoginRequiredMixin, DetailView):
+    model = Equipment
+    template_name = 'tickets/equipment_detail.html'
+   
+# Tickets 
+class TicketListView(LoginRequiredMixin, ListView):
+    model = Tickets
+    template_name = 'tickets/ticket_list.html'
+    
+class TicketDetailView(LoginRequiredMixin, DetailView):
+    model = Tickets
+    template_name = 'tickets/ticket_detail.html'
+    
+class TicketCreateView(LoginRequiredMixin, CreateView):
+    model = Tickets
+    template_name = 'tickets/ticket_form.html'
+    fields = ['equipment', 'title', 'description']
+    success_url = reverse_lazy('ticket_list')
+    
+    def form_valid(self, form):
+        form.instance.reporter = self.request.user
+        return super().form_valid(form)
+    
